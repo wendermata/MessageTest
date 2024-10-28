da0d431c3e02691ace451662ea02ee16d094b4a1
Wender Mata <wender.mata@gmail.com>
2024-10-28 03:05:31 -0300

    feat: add gitignore

--- obj/Debug/net8.0/MessagesTest.AssemblyInfo.cs
+++ obj/Debug/net8.0/MessagesTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MessagesTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44ef59e3a5b7408b3786cca094881fcee4468629")]
 [assembly: System.Reflection.AssemblyProductAttribute("MessagesTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MessagesTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
